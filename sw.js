ce1ed56264fb99d47369fe173b5094d47667d72d
TorredeControlCSS <pedregaljuandiaz@gmail.com>
2025-11-25 04:50:30 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('install', e=> self.skipWaiting());
-self.addEventListener('activate', e=> self.clients.claim());
-self.addEventListener('fetch', ()=>{});
+// minimal SW
+const VERSION = 'v3';
+self.addEventListener('install', (e)=>{ self.skipWaiting(); });
+self.addEventListener('activate', (e)=>{ clients.claim(); });
+self.addEventListener('fetch', (e)=>{
+  // passthrough; rely on network (no caching of API to avoid stale data)
+});
